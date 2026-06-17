3101e59806454875aefe35da5d81bfe2dc6225d1
shivanshyadav0909 <shivanshyadav9005@gmail.com>
2026-06-17 09:25:07 +0530

    Day-17 uploaded.

---
+++ Day_17/day17_3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    int n1, n2;
+    printf("Enter size of first array: ");
+    scanf("%d", &n1);
+    int arr1[n1];
+    for(int i=0; i<n1; i++) scanf("%d", &arr1[i]);
+
+    printf("Enter size of second array: ");
+    scanf("%d", &n2);
+    int arr2[n2];
+    for(int i=0; i<n2; i++) scanf("%d", &arr2[i]);
+
+    printf("Intersection: ");
+    for(int i=0; i<n1; i++) {
+        for(int j=0; j<n2; j++) {
+            if(arr1[i]==arr2[j]) {
+                printf("%d ", arr1[i]);
+                break;
+            }
+        }
+    }
+    return 0;
+}
